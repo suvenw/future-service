70385d6a2f2f671bd156a10658730c3068f97bcf
suven <suvenw@163.com>
2023-06-26 15:27:58 +0800

    微服务架构搭建 gateway模块

---
+++ future-gateway/src/main/java/com/sixeco/framework/gateway/filter/CustomWhiteFilter.java
@@ -0,0 +1,36 @@
+package com.sixeco.framework.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomWhiteFilter implements GlobalFilter {
+
+    private Logger logger = LoggerFactory.getLogger(CustomFilterFactory.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        logger.info("=========================请求进入filter=========================");
+
+        ServerHttpRequest request = exchange.getRequest();
+        String requestPath = request.getPath().toString();
+
+        boolean allowedPath = true;
+        if(allowedPath) {
+            try {
+                return chain.filter(exchange);
+            }catch (Exception e){
+                logger.info("=========================请求进入filter Exception=========================",e);
+            }
+
+        }
+        return null;
+    }
+}
